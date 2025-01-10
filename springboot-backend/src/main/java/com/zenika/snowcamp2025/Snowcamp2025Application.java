bd0abe39512760c843ef93a983cce9db5db0aa88
Ben Einaudi <antechrestos@gmail.com>
2025-01-10 19:26:02 +0100

    add springboot

---
+++ springboot-backend/src/main/java/com/zenika/snowcamp2025/Snowcamp2025Application.java
@@ -0,0 +1,13 @@
+package com.zenika.snowcamp2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Snowcamp2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Snowcamp2025Application.class, args);
+	}
+
+}
---
+++ springboot-backend/src/test/java/com/zenika/snowcamp2025/Snowcamp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.snowcamp2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Snowcamp2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
